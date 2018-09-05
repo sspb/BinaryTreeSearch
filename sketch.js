3fe19482015c4c2f1fd240eb5f427c917d966837
sspboyd <sspboyd@gmail.com>
2018-09-04 22:40:38 -0400

    working but no way to visualize the tree yet. About to split the code up into separate files

--- sketch.js
+++ sketch.js
@@ -5,23 +5,45 @@ function setup() {
     noCanvas();
     tree = new Tree();
     tree.addValue(5);
+    tree.addValue(3);
+    tree.addValue(7);
+    tree.addValue(6);
 
     console.log(tree);
 }
 
-function Tree(){
-	this.root = null;
+function Tree() {
+    this.root = null;
 }
 
-Tree.prototype.addValue = function(val){
+Tree.prototype.addValue = function(val) {
     var n = new Node(val);
-	if(this.root == null){
-		this.root = n;
-	}
+    if (this.root == null) {
+        this.root = n;
+    } else {
+        this.root.addNode(n);
+    }
+}
+
+
+Node.prototype.addNode = function(n) {
+    if (n.value < this.value) {
+        if (this.left == null) {
+            this.left = n;
+        } else {
+            this.left.addNode(n);
+        }
+    } else if(n.value > this.value){
+        if (this.right == null) {
+            this.right = n;
+        } else {
+            this.right.addNode(n);
+        }
+    }
 }
 
 function Node(val) {
-	this.value = val;
+    this.value = val;
     this.left = null;
     this.right = null;
 }
