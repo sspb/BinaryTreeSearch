b2997113c2fdd9e5029b1bf7d758f1f713aa6344
sspboyd <sspboyd@gmail.com>
2018-09-04 23:51:14 -0400

    Broke out into separate files. Added traversing the tree and search functions.

--- sketch.js
+++ sketch.js
@@ -4,46 +4,10 @@ var tree
 function setup() {
     noCanvas();
     tree = new Tree();
-    tree.addValue(5);
-    tree.addValue(3);
-    tree.addValue(7);
-    tree.addValue(6);
+    for (var i = 0; i < 20; i++) {
+        tree.addValue(floor(random(0, 1000)));
+    }
 
     console.log(tree);
-}
-
-function Tree() {
-    this.root = null;
-}
-
-Tree.prototype.addValue = function(val) {
-    var n = new Node(val);
-    if (this.root == null) {
-        this.root = n;
-    } else {
-        this.root.addNode(n);
-    }
-}
-
-
-Node.prototype.addNode = function(n) {
-    if (n.value < this.value) {
-        if (this.left == null) {
-            this.left = n;
-        } else {
-            this.left.addNode(n);
-        }
-    } else if(n.value > this.value){
-        if (this.right == null) {
-            this.right = n;
-        } else {
-            this.right.addNode(n);
-        }
-    }
-}
-
-function Node(val) {
-    this.value = val;
-    this.left = null;
-    this.right = null;
+    tree.traverse();
 }
